f09b2f3e6a618ed530046d6dd398f1be1da97936
Saurabh Kuntal <wintersoldier110@gmail.com>
2024-12-24 06:58:40 +0000

    basic react

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>Chai aur React with vite | Saurabh Kuntal | vite install and linux command  </h1>
+  )
+}
+
+export default App
