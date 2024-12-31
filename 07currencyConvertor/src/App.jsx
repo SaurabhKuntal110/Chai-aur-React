6c7d97964120aacb5ac952113cc814310cc0ccd7
Saurabh Kuntal <wintersoldier110@gmail.com>
2024-12-31 10:51:25 +0000

    currency convertor

---
+++ 07currencyConvertor/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+  return (
+    <>
+      <h1 className='text-3xl bg-orange-500  mx-3 my-10 '>Currency Convertor</h1>
+    </>
+  )
+}
+
+export default App
