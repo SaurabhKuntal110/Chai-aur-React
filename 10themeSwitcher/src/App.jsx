7ce7671cf581af81d221eb35df53d4fa9974eb2d
Saurabh Kuntal <wintersoldier110@gmail.com>
2025-01-23 06:45:13 +0000

    themeToggle Complete

--- 10themeSwitcher/src/App.jsx
+++ 10themeSwitcher/src/App.jsx
@@ -1,24 +1,41 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
+import { ThemeProvider } from './contexts/Theme'
 
 import './App.css'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 
 function App() {
+  const [themeMode, setThemeMode] = useState('light')
+
+  const lightTheme = () =>{
+    setThemeMode('light')
+  }
+  const darkTheme = () =>{
+    setThemeMode('dark')
+  }
+
+  useEffect(() => {
+    const backGround = document.querySelector('html')
+    backGround.classList.remove('light', 'dark')
+    backGround.classList.add(themeMode)
+  }, [themeMode])
   
 
   return (
-    
-<div className="flex flex-wrap min-h-screen items-center">
+    <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
                 <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        {/* theme btn */}
+                      <ThemeBtn />
                     </div>
 
                     <div className="w-full max-w-sm mx-auto">
-                       {/* card */}
+                      <Card />
                     </div>
                 </div>
-            </div>
-
+      </div>
+    </ThemeProvider>
   )
 }
 
